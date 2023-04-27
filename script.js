721d4a18c7e8625ce644ea875d5f1f72a960b09e
Patrick Zhou <patrickchang44543@gmail.com>
2023-04-26 22:02:28 -0700

    added userlist collapse

--- script.js
+++ script.js
@@ -11,13 +11,41 @@
 
 let collapsed = false;
 let origWidth;
-let channels, guildsList;
+let channels, guildsList, userList, origUserWidth;
+
+let prevUrl = undefined;
+setInterval(() => {
+    const currUrl = window.location.href.split("/")[4];
+    if (currUrl != prevUrl) {
+        setTimeout(() => {
+            if (document.querySelectorAll(".container-2o3qEW").length > 0) {
+                userList = document.querySelectorAll(".container-2o3qEW")[0];
+                userList.style.transition = "width 0.2s";
+                origUserWidth = userList.offsetWidth;
+                userList.style.width = 0 + "px";
+                userList.onmouseleave = () => {
+                    userList.style.width = 0 + "px";
+                };
+            }
+        }, 500);
+        prevUrl = currUrl;
+    }
+}, 60);
 
 setTimeout(() => {
     channels = document.querySelectorAll(".sidebar-1tnWFu")[0];
     channels.style.transition = "width 0.2s";
     origWidth = channels.offsetWidth;
     channels.style.width = 0 + "px";
+    if (document.querySelectorAll(".container-2o3qEW").length > 0) {
+        userList = document.querySelectorAll(".container-2o3qEW")[0];
+        userList.style.transition = "width 0.2s";
+        origUserWidth = userList.offsetWidth;
+        userList.style.width = 0 + "px";
+        userList.onmouseleave = () => {
+            userList.style.width = 0 + "px";
+        };
+    }
 
     window.onmousemove = (event) => {
         guildsList = document.querySelectorAll(".guilds-2JjMmN")[0];
@@ -26,6 +54,12 @@ setTimeout(() => {
             event.pageX <= guildsList.offsetLeft + guildsList.offsetWidth + 20
         ) {
             channels.style.width = origWidth + "px";
+        }
+        if (
+            event.pageX >= window.innerWidth - 20 &&
+            document.querySelectorAll(".container-2o3qEW").length > 0
+        ) {
+            userList.style.width = origUserWidth + "px";
         }
     };
 
