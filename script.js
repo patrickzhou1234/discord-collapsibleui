309a574f8d503359f7736458cf3e378cfeb52871
Patrick Zhou <patrickchang44543@gmail.com>
2023-06-13 22:10:19 -0700

    added user profile

--- script.js
+++ script.js
@@ -13,19 +13,27 @@ let collapsed = false;
 let origWidth;
 let channels, guildsList, userList, origUserWidth;
 
-let prevUrl = undefined;
+let prevUrl = "/////";
+
+function userHandle() {
+    userList.style.transition = "width 0.2s";
+    origUserWidth = userList.offsetWidth;
+    userList.style.width = 0 + "px";
+    userList.onmouseleave = () => {
+        userList.style.width = 0 + "px";
+    };
+}
+
 setInterval(() => {
-    const currUrl = window.location.href.split("/")[4];
-    if (currUrl != prevUrl) {
-        if (document.querySelectorAll(".container-2o3qEW").length > 0) {
-            userList = document.querySelectorAll(".container-2o3qEW")[0];
-            userList.style.transition = "width 0.2s";
-            origUserWidth = userList.offsetWidth;
-            userList.style.width = 0 + "px";
-            userList.onmouseleave = () => {
-                userList.style.width = 0 + "px";
-            };
-        }
+    const currUrl = window.location.href.split("/");
+    if (currUrl[4] != prevUrl[4] && document.querySelectorAll(".container-2o3qEW").length > 0) {
+        userList = document.querySelectorAll(".container-2o3qEW")[0];
+        userHandle();
+        prevUrl = currUrl;
+    }
+    if (currUrl[4] == "@me" && currUrl[5] != prevUrl[5] && document.querySelectorAll(".profilePanel-2VBkh8").length > 0) {
+        userList = document.querySelectorAll(".profilePanel-2VBkh8")[0];
+        userHandle();
         prevUrl = currUrl;
     }
 }, 60);
@@ -35,28 +43,24 @@ setTimeout(() => {
     channels.style.transition = "width 0.2s";
     origWidth = channels.offsetWidth;
     channels.style.width = 0 + "px";
+    var found = false;
     if (document.querySelectorAll(".container-2o3qEW").length > 0) {
         userList = document.querySelectorAll(".container-2o3qEW")[0];
-        userList.style.transition = "width 0.2s";
-        origUserWidth = userList.offsetWidth;
-        userList.style.width = 0 + "px";
-        userList.onmouseleave = () => {
-            userList.style.width = 0 + "px";
-        };
+        found = true;
     }
-
+    if (document.querySelectorAll(".profilePanel-2VBkh8") > 0) {
+        userList = document.querySelectorAll(".profilePanel-2VBkh8")[0];
+        found = true;
+    }
+    if (found) {
+        userHandle();
+    }
     window.onmousemove = (event) => {
         guildsList = document.querySelectorAll(".guilds-2JjMmN")[0];
-        if (
-            event.pageX >= guildsList.offsetLeft + guildsList.offsetWidth &&
-            event.pageX <= guildsList.offsetLeft + guildsList.offsetWidth + 20
-        ) {
+        if (event.pageX >= guildsList.offsetLeft + guildsList.offsetWidth && event.pageX <= guildsList.offsetLeft + guildsList.offsetWidth + 20) {
             channels.style.width = origWidth + "px";
         }
-        if (
-            event.pageX >= window.innerWidth - 20 &&
-            document.querySelectorAll(".container-2o3qEW").length > 0
-        ) {
+        if (event.pageX >= window.innerWidth - 20 && (document.querySelectorAll(".profilePanel-2VBkh8").length > 0 || document.querySelectorAll(".container-2o3qEW").length > 0)) {
             userList.style.width = origUserWidth + "px";
         }
     };
